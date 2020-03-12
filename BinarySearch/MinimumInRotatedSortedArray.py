3af0e290460cc9c8bca2c150bf1dee47bacbe87d
piergiorgioladisa <44639444+piergiorgioladisa@users.noreply.github.com>
2020-03-12 20:22:41 +0100

    Create MinimumInRotatedSortedArray.py

---
+++ BinarySearch/MinimumInRotatedSortedArray.py
@@ -0,0 +1,40 @@
+"""
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+Find the minimum element.
+You may assume no duplicate exists in the array.
+
+Example 1:
+
+Input: [3,4,5,1,2] 
+Output: 1
+
+Example 2:
+
+Input: [4,5,6,7,0,1,2]
+Output: 0
+
+
+"""
+
+
+def findMin(nums: List[int]) -> int:
+        
+        left, right = 0, len(nums)-1
+        
+        if len(nums) == 0:
+            return -1
+        if len(nums) == 1:
+            return nums[0]
+        
+        while left < right:         
+            mid = (left + right)//2
+            if nums[mid] > nums[mid+1]:
+                return nums[mid + 1]
+            elif nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return nums[left]
+        
