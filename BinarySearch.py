a8615a72069fe8aeec80a7c7a44f8202cc071e3a
piergiorgioladisa <44639444+piergiorgioladisa@users.noreply.github.com>
2020-03-12 17:14:01 +0100

    Create BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,20 @@
+"""
+Search a target number inside a sorted array and return its index.
+If it's not inside, return -1
+"""
+
+def search(self, nums: List[int], target: int) -> int:
+        
+        mid_index = len(nums) // 2
+        
+        if nums[mid_index] == target:
+            return mid_index
+        elif nums[mid_index] > target:
+            for index in range(mid_index):
+                if nums[index] == target:
+                    return index
+        elif nums[mid_index] < target:
+            for index in range(mid_index+1, len(nums)):
+                if nums[index] == target:
+                    return index
+        return -1
